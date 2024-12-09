91fa83e330dedcacd0d9e9c45d06bf05163ac4d7
MisaghMomeniB <jenabemisagh@gmail.com>
2024-12-09 08:12:05 +0330

    Import Library

---
+++ main.py
@@ -0,0 +1,6 @@
+import sys
+from PyQt5.QtWidgets import (
+    QApplication, QMainWindow, QTextEdit, QMenuBar, QAction, QFileDialog, QMessageBox, QInputDialog
+)
+from PyQt5.QtGui import QFont, QIcon
+from fpdf import FPDF
